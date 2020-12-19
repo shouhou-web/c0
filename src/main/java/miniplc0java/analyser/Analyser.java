02d50145482d8626ad71dabccb9eef92f1df41f5
ShouHou <18373337@buaa.edu.cn>
2020-12-19 18:22:15 +0800

    analyser 初始化

---
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -0,0 +1,147 @@
+package miniplc0java.analyser;
+
+import miniplc0java.error.AnalyzeError;
+import miniplc0java.error.CompileError;
+import miniplc0java.error.ErrorCode;
+import miniplc0java.error.ExpectedTokenError;
+import miniplc0java.error.TokenizeError;
+import miniplc0java.instruction.Instruction;
+import miniplc0java.instruction.Operation;
+import miniplc0java.tokenizer.Token;
+import miniplc0java.tokenizer.TokenType;
+import miniplc0java.tokenizer.Tokenizer;
+import miniplc0java.util.Pos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class Analyser {
+    Tokenizer tokenizer;
+
+    /**
+     * 待修改
+     */
+    ArrayList<Instruction> instructions;
+
+    /**
+     * 当前偷看的 token
+     */
+    Token peekedToken = null;
+
+    /**
+     * 符号表
+     */
+    LinkedHashMap<String, SymbolEntry> symbolTable = new LinkedHashMap<>();
+
+    /**
+     * 下一个变量的栈偏移
+     */
+    int nextOffset = 0;
+
+    public Analyser(Tokenizer tokenizer) {
+        this.tokenizer = tokenizer;
+        this.instructions = new ArrayList<>();
+    }
+
+    public List<Instruction> analyse() throws CompileError {
+        analyseProgram();
+        return instructions;
+    }
+
+    /**
+     * 查看下一个 Token
+     *
+     * @return
+     * @throws TokenizeError
+     */
+    private Token peek() throws TokenizeError {
+        if (peekedToken == null) {
+            peekedToken = tokenizer.nextToken();
+        }
+        return peekedToken;
+    }
+
+    /**
+     * 获取下一个 Token
+     *
+     * @return
+     * @throws TokenizeError
+     */
+    private Token next() throws TokenizeError {
+        if (peekedToken != null) {
+            var token = peekedToken;
+            peekedToken = null;
+            return token;
+        } else {
+            return tokenizer.nextToken();
+        }
+    }
+
+    /**
+     * 如果下一个 token 的类型是 tt，则返回 true
+     *
+     * @param tt
+     * @return
+     * @throws TokenizeError
+     */
+    private boolean check(TokenType tt) throws TokenizeError {
+        var token = peek();
+        return token.getTokenType() == tt;
+    }
+
+    /**
+     * 如果下一个 token 的类型是 tt，则前进一个 token 并返回这个 token
+     *
+     * @param tt 类型
+     * @return 如果匹配则返回这个 token，否则返回 null
+     * @throws TokenizeError
+     */
+    private Token nextIf(TokenType tt) throws TokenizeError {
+        var token = peek();
+        if (token.getTokenType() == tt) {
+            return next();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 如果下一个 token 的类型是 tt，则前进一个 token 并返回，否则抛出异常
+     *
+     * @param tt 类型
+     * @return 这个 token
+     * @throws CompileError 如果类型不匹配
+     */
+    private Token expect(TokenType tt) throws CompileError {
+        var token = peek();
+        if (token.getTokenType() == tt) {
+            return next();
+        } else {
+            throw new ExpectedTokenError(tt, token);
+        }
+    }
+
+    /**
+     * 获取下一个变量的栈偏移
+     *
+     * @return
+     */
+    private int getNextVariableOffset() {
+        return this.nextOffset++;
+    }
+
+    private void analyseProgram() throws CompileError {
+        // 程序 -> 'begin' 主过程 'end'
+        // 示例函数，示例如何调用子程序
+        // 'begin'
+//        expect(TokenType.Begin);
+//
+//        analyseMain();
+//
+//        // 'end'
+//        expect(TokenType.End);
+//        expect(TokenType.EOF);
+    }
+}
